3e449b04f9d54fcab1a02ca5ffd5c457eb9ba512
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-09-03 13:17:35 -0600

    Revert "working music rec component"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
-import MusicComponent from './components/musicRec'
 
 class App extends React.Component {
- 
   render() {
     const { name } = this.props;
-
     return (
       <>
-       <MusicComponent />
+        <h1>
+          Hello {name}
+        </h1>
       </>
     );
   }
 }
 
 export default hot(App);
-
-
