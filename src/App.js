a27c179b2caaa7fff32f166fe9ebad4a8bef61ee
Adam Seery <adam@Adams-MacBook-Air.local>
2022-09-03 13:11:27 -0600

    merging Hina's search comp changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import SearchComponent from './components/SearchComponent.js'
 import MusicComponent from './components/musicRec'
 
 class App extends React.Component {
- 
+
   render() {
     const { name } = this.props;
 
     return (
       <>
-       <MusicComponent />
+        <h1>
+          Beat Books
+        </h1>
+        <SearchComponent />
+        <MusicComponent />
       </>
     );
   }
 }
 
 export default hot(App);
-
-
