5a1f4a3e89d51b771203618691b05282d20d7154
Adam Seery <adam@Adams-MacBook-Air.local>
2022-09-05 16:40:12 -0600

    added logo png

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { hot } from 'react-hot-loader/root';
 import SearchComponent from './components/SearchComponent.js'
 import MusicComponent from './components/musicRec'
 import LoadingComponent from "./components/LoadingComponent.js";
+import logo from "../src/assets/logo.png"
 
 class App extends React.Component {
 
@@ -11,10 +12,8 @@ class App extends React.Component {
 
     return (
       <>
-        <h1>
-          Beat Books
-        </h1>
-        {/* <SearchComponent /> */}
+       <div id="logo-container"> <img id="logo" src={logo}></img></div>
+        <SearchComponent />
         <LoadingComponent/>
       </>
     );
