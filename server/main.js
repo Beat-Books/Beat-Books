5283670ed43b34107ac82b6398a841ebe39ff60a
sagarvxyz <sagar.velagala@gmail.com>
2022-09-07 14:56:51 -0400

    feat: get spotify auth code

--- server/main.js
+++ server/main.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const musicRouter = require('./routes/music');
 const app = express();
 const PORT = 3000;
 const dotenv = require('dotenv').config();
@@ -14,6 +13,7 @@ app.use(cookieParser());
 // Import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const musicRouter = require('./routes/music');
 
 // Connect to MongoDB
 mongoose
@@ -28,6 +28,7 @@ mongoose
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/music', musicRouter);
 
 app.get('/api', (req, res) => {
   return res.status(200).json({ done: true });
@@ -44,13 +45,6 @@ app.use((err, req, res, next) => {
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
-
-// NOTE: for testing purposes
-app.get('/', (req, res) => {
-  res.status(200).end();
-})
-
-app.use('/api/music', musicRouter);
 
 /* changes default behavior to "find first avaible port" in testing. This allows
 multiple tests to be run in paralell on different ports */
--- server/routes/music.js
+++ server/routes/music.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const musicController = require('../controllers/musicController');
 const router = express.Router();
+
+router.get('/spotify/auth', musicController.AuthorizeSpotifyUser, 
+  (req, res) => {
+    res.redirect(res.locals.authURL).status(302);
+  })
+
+// Note: req.query.code is the Spotify auth code needed to get an access token
+router.get('/spotify/auth/token', (req, res) => {
+  console.log('/spotify/auth/token token recieved');
+  res.status(200).send(req.query.code);
+});
 
 router.get('/', (req, res) => {
   console.log('music router is working');
